8c46c0444fe58084c6d4505413c7597dd8fcdb7d
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-16 21:30:10 -0600

    TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 
 import React from 'react';
-import {
-  StyleSheet,
-} from 'react-native';
+import {  StyleSheet, View,  TouchableWithoutFeedback } from 'react-native';
+
 import Form from './components/Form';
+
+
+
+
+const ocultarTeclado = () => {
+  Keyboard.dismiss();
+}
+
+
 
 const App = () => {
   return (
-    <>
-    <Form/>
-    </>
+    <TouchableWithoutFeedback onPress={ () => ocultarTeclado() }>
+    <View style={[styles.app ]}>
+        <View style={styles.contenido}>
+          
+          <Form/>
+
+        </View>
+    </View>
+</TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
-
+  app: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
 });
 
 export default App;
